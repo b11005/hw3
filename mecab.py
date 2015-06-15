45c715148bc318a46fd28ef3a8d05a0ceca4814c
b11005 <instantjoy3@gmail.com>
2015-06-15 19:29:45 +0900

    まだ文字化け;(

--- mecab.py
+++ mecab.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
-# coding: utf-8
+
 import MeCab
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 words={}
 def getNoun(text):
-    tagger=MeCab.Tagger('Ochasen')
+    tagger=MeCab.Tagger('-Ochasen')
     node=tagger.parseToNode(text.encode('utf-8'))
     while node:
         noun=node.feature.split(',')[0]
-        if noun==u'です':
+        if noun==u'名詞':
             words.setdefault(node.surface,0)
-            words[node.serface]+=1
+            words[node.surface]+=1
         node=node.next
     return words
     
 f=open('sub.txt')
-f1=open('collocation.txt','w')
+#f1=open('collocation.txt','w')
 for line in f:
     line1,line2=line.strip().split('\t')
     d=getNoun(line2)
-    for k,v in sorted(words.items(), key=lambda x:x[1]):
-        f1.write(k, v)
+    for k,v in words.items():
+        print k, v
     
 f.close()    
-f1.close()
+#f1.close()
