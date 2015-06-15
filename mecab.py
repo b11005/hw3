1c6b0149ce6c7922d2dac8c926b3a81e05b2b853
b11005 <instantjoy3@gmail.com>
2015-06-15 21:49:42 +0900

    文字化け解消

--- mecab.py
+++ mecab.py
@@ -7,11 +7,12 @@ sys.setdefaultencoding('utf-8')
 
 words={}
 def getNoun(text):
-    tagger=MeCab.Tagger('-Ochasen')
+    tagger=MeCab.Tagger('-Owakati')
+    tagger.parse('')
     node=tagger.parseToNode(text.encode('utf-8'))
     while node:
         noun=node.feature.split(',')[0]
-        if noun==u'名詞':
+        if noun==r'名詞':
             words.setdefault(node.surface,0)
             words[node.surface]+=1
         node=node.next
@@ -23,7 +24,7 @@ for line in f:
     line1,line2=line.strip().split('\t')
     d=getNoun(line2)
     for k,v in words.items():
-        print k, v
+        print k.decode('utf-8'), v
     
 f.close()    
 #f1.close()
