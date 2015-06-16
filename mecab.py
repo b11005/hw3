ac70a4491125cfec4a8851076c7e7819f83b12c2
b11005 <instantjoy3@gmail.com>
2015-06-16 20:19:49 +0900

    文字化け解消

--- mecab.py
+++ mecab.py
@@ -10,7 +10,6 @@ def getNoun(text):#単語に区切り、単語が何回でてくるか数える
     tagger=MeCab.Tagger('-Owakati')#MeCabの設定
     tagger.parse('')#parseToNodeのバグ回避
     node=tagger.parseToNode(text.encode('utf-8'))
-#    node=tagger.parse(text)
     while node:#ハッシュに入れて数える
         noun=node.feature.split(",")[0]
         if noun==u'名詞':
@@ -18,15 +17,17 @@ def getNoun(text):#単語に区切り、単語が何回でてくるか数える
             words[node.surface]+=1
         node=node.next
     return words
-    
+
 f=open('sub.txt')
-#f1=open('collocation.txt','w')
+f1=open('collocation.txt','w')
 for line in f:
     line1,line2=line.strip().split('\t')
     d=getNoun(line2)
-    for k,v in sorted(words.items(), key=lambda x:x[1], reverse=True):
-        print k.decode('utf-8'), v,
-    print
+    for k,v in sorted(words.items(), key=lambda x:int(x[1]), reverse=True):
+        f1.write(k.decode('utf-8'))
+        f1.write('\t')
+        f1.write(str(v))
+        f1.write('\n')
     
 f.close()    
-#f1.close()
+f1.close()
