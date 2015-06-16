510c46328038e1e8ec2f688b0963d6369694c50b
b11005 <instantjoy3@gmail.com>
2015-06-16 22:20:44 +0900

    半角記号と半角数字が混ざってるけど、出力可

--- mecab.py
+++ mecab.py
@@ -18,16 +18,19 @@ def getNoun(text):#単語に区切り、単語が何回でてくるか数える
         node=node.next
     return words
 
-f=open('sub.txt')
-f1=open('collocation.txt','w')
+f=open('sub_pages.txt')
+#f1=open('collocation.txt','w')
 for line in f:
     line1,line2=line.strip().split('\t')
     d=getNoun(line2)
-    for k,v in sorted(words.items(), key=lambda x:int(x[1]), reverse=True):
+for k,v in sorted(words.items(), key=lambda x:x[1], reverse=True):
+    print k, v
+
+"""    for k,v in sorted(words.items(), key=lambda x:x[1], reverse=True):
         f1.write(k.decode('utf-8'))
         f1.write('\t')
         f1.write(str(v))
-        f1.write('\n')
+        f1.write('\n')"""
     
 f.close()    
-f1.close()
+#f1.close()
