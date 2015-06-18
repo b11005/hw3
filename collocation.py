f918d9c118b1c9b2fae4549f2bfca2fa7fbfe17a
b11005 <instantjoy3@gmail.com>
2015-06-18 13:07:39 +0900

    collocation list up

--- collocation.py
+++ collocation.py
@@ -18,11 +18,17 @@ def getNoun(text):#単語に区切り、単語が何回でてくるか数える
         node=node.next
     return words
 
-f=open('sub_pages.txt')
+f=open('pages.txt')
+f1=open('collocation.txt','w')
 for line in f:
     line1,line2=line.strip().split('\t')
     d=getNoun(line2)
 for k,v in sorted(words.items(), key=lambda x:x[1], reverse=True):
-    if v>50: print k, v
+    if v>50: 
+        f1.write(k.decode('utf-8'))
+        f1.write('\t')
+        f1.write(str(v))
+        f1.write('\n')
 
 f.close()    
+f1.close()
